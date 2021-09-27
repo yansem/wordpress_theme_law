1a9d36278cae95ff679c6d9c269568734971d2b4
SEMA <janchevsem@mail.ru>
2021-09-27 14:49:34 +0500

    кастомизация слайдера-создание

--- wp-content/themes/law/framework-customizations/extensions/media/extensions/slider/extensions/flex-slider/config.php
+++ wp-content/themes/law/framework-customizations/extensions/media/extensions/slider/extensions/flex-slider/config.php
@@ -1,2 +1,3 @@
 <?php if (!defined('FW')) die('Forbidden');
-$cfg['multimedia_types'] = array('image','video');
+$cfg['population_methods'] = array('custom');
+$cfg['multimedia_types'] = array('image');
--- wp-content/themes/law/framework-customizations/extensions/media/extensions/slider/extensions/flex-slider/manifest.php
+++ wp-content/themes/law/framework-customizations/extensions/media/extensions/slider/extensions/flex-slider/manifest.php
@@ -1,9 +1,2 @@
 <?php if (!defined('FW')) die('Forbidden');
-
-$manifest = array();
-
-$manifest['name']        = __( 'Bx-Slider', 'fw' );
-$manifest['description'] = __( 'Bx-Slider', 'fw' );
-$manifest['version'] = '1.0.0';
-$manifest['display'] = 'slider';
 $manifest['standalone'] = true;
