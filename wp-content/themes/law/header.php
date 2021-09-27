8ce6c3cecb1ac56f67a7dea02e29b69f0858d75a
SEMA <janchevsem@mail.ru>
2021-09-27 09:58:00 +0500

    вывод меню

--- wp-content/themes/law/header.php
+++ wp-content/themes/law/header.php
@@ -31,24 +31,31 @@
                         <div id="fh5co-logo"><a href="index.html">Law<span>.</span></a></div>
                     </div>
                     <div class="col-xs-10 text-right menu-1">
-                        <ul>
-                            <li class="active"><a href="index.html">Home</a></li>
-                            <li><a href="practice.html">Practice Areas</a></li>
-                            <li><a href="won.html">Won Cases</a></li>
-                            <li class="has-dropdown">
-                                <a href="blog.html">Blog</a>
-                                <ul class="dropdown">
-                                    <li><a href="#">Web Design</a></li>
-                                    <li><a href="#">eCommerce</a></li>
-                                    <li><a href="#">Branding</a></li>
-                                    <li><a href="#">API</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="contact.html">Contact</a></li>
-                            <li class="btn-cta"><a href="#"><span>Login</span></a></li>
-                            <li class="btn-cta"><a href="#"><span>Sign Up</span></a></li>
-                        </ul>
+                        <?php wp_nav_menu( [
+
+                                'theme_location' => 'header-menu',
+                                'container' => false,
+
+                        ] ); ?>
+
+<!--                        <ul>-->
+<!--                            <li class="active"><a href="index.html">Home</a></li>-->
+<!--                            <li><a href="practice.html">Practice Areas</a></li>-->
+<!--                            <li><a href="won.html">Won Cases</a></li>-->
+<!--                            <li class="has-dropdown">-->
+<!--                                <a href="blog.html">Blog</a>-->
+<!--                                <ul class="dropdown">-->
+<!--                                    <li><a href="#">Web Design</a></li>-->
+<!--                                    <li><a href="#">eCommerce</a></li>-->
+<!--                                    <li><a href="#">Branding</a></li>-->
+<!--                                    <li><a href="#">API</a></li>-->
+<!--                                </ul>-->
+<!--                            </li>-->
+<!--                            <li><a href="about.html">About</a></li>-->
+<!--                            <li><a href="contact.html">Contact</a></li>-->
+<!--                            <li class="btn-cta"><a href="#"><span>Login</span></a></li>-->
+<!--                            <li class="btn-cta"><a href="#"><span>Sign Up</span></a></li>-->
+<!--                        </ul>-->
                     </div>
                 </div>
 
