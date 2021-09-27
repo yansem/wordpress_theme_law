9b9cc495bd5e06819f4d307d71bb6f3e877e2daf
SEMA <janchevsem@mail.ru>
2021-09-27 15:19:43 +0500

    кастомизация слайдера-debug

--- wp-content/themes/law/framework-customizations/extensions/media/extensions/slider/extensions/flex-slider/options/custom.php
+++ wp-content/themes/law/framework-customizations/extensions/media/extensions/slider/extensions/flex-slider/options/custom.php
@@ -1,12 +1,18 @@
 <?php if ( ! defined( 'FW' ) ) {
 	die( 'Forbidden' );
 }
-//$options = array(
-//	'subtitle' => array(
-//		'type'  => 'text',
-//		'label' => __( 'Subtitle', 'fw' ),
-//		'value' => '',
-//		'desc'  => __( 'Choose a subtitle for your slide.', 'fw' )
-//	)
-//);
+$options = array(
+	'link' => array(
+		'type'  => 'text',
+		'label' => __( 'Link', 'fw' ),
+		'value' => '',
+		'desc'  => __( 'Choose a link for your slide.', 'fw' )
+	),
+    'link_title' => array(
+        'type'  => 'text',
+        'label' => __( 'Link Title', 'fw' ),
+        'value' => '',
+        'desc'  => __( 'Choose a link title for your slide.', 'fw' )
+    )
+);
 
