97c059bdc7690366ae63d52375dc6a7ddc1efb8c
SEMA <janchevsem@mail.ru>
2021-09-27 23:37:11 +0500

    шорткод span-column options 2

--- wp-content/themes/law/framework-customizations/extensions/shortcodes/shortcodes/column/options.php
+++ wp-content/themes/law/framework-customizations/extensions/shortcodes/shortcodes/column/options.php
@@ -3,28 +3,14 @@
 }
 
 $options = array(
-	'span_icon'       => array(
-		'type' => 'icon',
-		'label' => __( 'Span Icon', 'fw' )
-	),
-	'span_content'    => array(
-		'type'  => 'text',
-		'label' => __( 'Span Content', 'fw' ),
-		'desc'  => __( 'Span Content', 'fw' ),
-	),
-    'span_class'    => array(
+    'col_class'    => array(
         'type'  => 'text',
-        'label' => __( 'Span Class', 'fw' ),
-        'desc'  => __( 'Span Class', 'fw' ),
+        'label' => __( 'Column Class', 'fw' ),
+        'desc'  => __( 'Column Class', 'fw' ),
     ),
-    'span_id'    => array(
+    'col_id'    => array(
         'type'  => 'text',
-        'label' => __( 'Span ID', 'fw' ),
-        'desc'  => __( 'Span ID', 'fw' ),
-    ),
-    'span_data_attrs'    => array(
-        'type'  => 'text',
-        'label' => __( 'Attributes', 'fw' ),
-        'desc'  => __( 'Attributes', 'fw' ),
+        'label' => __( 'Column ID', 'fw' ),
+        'desc'  => __( 'Column ID', 'fw' ),
     ),
 );
--- wp-content/themes/law/framework-customizations/extensions/shortcodes/shortcodes/column/views/view.php
+++ wp-content/themes/law/framework-customizations/extensions/shortcodes/shortcodes/column/views/view.php
@@ -2,6 +2,7 @@
 
 $class = fw_ext_builder_get_item_width('page-builder', $atts['width'] . '/frontend_class');
 ?>
-<div class="<?php echo esc_attr($class); ?>">
+<div class="<?php echo esc_attr($class); ?> <?php if(!empty($atts['col_class'])) echo $atts['col_class']; ?>"
+    <?php if(!empty($atts['col_id'])) echo "id='{$atts['col_id']}'"; ?>>
 	<?php echo do_shortcode($content); ?>
 </div>
