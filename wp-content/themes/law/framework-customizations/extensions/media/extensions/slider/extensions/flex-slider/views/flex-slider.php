09493294c8d08fa99c2d1801cb30bc7f7ea6a45a
SEMA <janchevsem@mail.ru>
2021-09-27 15:39:54 +0500

    кастомизация слайдера-вывод

--- wp-content/themes/law/framework-customizations/extensions/media/extensions/slider/extensions/flex-slider/views/flex-slider.php
+++ wp-content/themes/law/framework-customizations/extensions/media/extensions/slider/extensions/flex-slider/views/flex-slider.php
@@ -1,2 +1,28 @@
 <?php if (!defined('FW')) die('Forbidden'); ?>
-<?php debug($data['slides']); ?>
+
+<?php if($data['slides']): ?>
+    <aside id="fh5co-hero" class="js-fullheight">
+        <div class="flexslider js-fullheight">
+            <ul class="slides">
+                <?php foreach ($data['slides'] as $slide): ?>
+                <li style="background-image: url(<?php echo $slide['src']; ?>);">
+                    <div class="overlay-gradient"></div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+                                <div class="slider-text-inner">
+                                    <h1><?php echo $slide['title']; ?></h1>
+                                    <h2><?php echo $slide['desc']; ?></h2>
+                                    <?php if(!empty($slide['extra']['link']) && !empty($slide['extra']['link_title'])): ?>
+                                        <p><a class="btn btn-primary btn-lg" href="<?php echo $slide['extra']['link']; ?>"><?php echo $slide['extra']['link_title']; ?></a></p>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </aside>
+<?php endif; ?>
