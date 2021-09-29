d5e4fb94a3cc12c5fe3351895f30a5f5fe8d356f
SEMA <janchevsem@mail.ru>
2021-09-29 17:33:24 +0500

    page contact

--- wp-content/themes/law/page.php
+++ wp-content/themes/law/page.php
@@ -1,38 +1,20 @@
-<?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Law
- */
+<?php get_header(); ?>
 
-get_header();
-?>
+<div class="container-fluid p-0">
+    <div class="row">
+        <div class="col-md-12">
 
-	<main id="primary" class="site-main">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <!-- post -->
+                <?php the_content(''); ?>
+            <?php endwhile; ?>
+                <!-- post navigation -->
+            <?php else: ?>
+                <!-- no posts found -->
+            <?php endif; ?>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+        </div>
+    </div>
+</div>
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
